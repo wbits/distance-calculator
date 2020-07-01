e84e36c21e2573a8fad0471c6ba63c1573557a56
Dick Brouwers <dick@insided.nl>
2020-07-01 18:41:52 +0200

    green

--- src/DistanceCalculator.php
+++ src/DistanceCalculator.php
@@ -6,9 +6,9 @@ namespace Assignment;
 
 final class DistanceCalculator
 {
-
-    public function calculate(array $distance1, array $distance2, string $string)
+    public function calculate(array $distance1, array $distance2, string $string): float
     {
+        return $distance1['distance'] + $distance2['distance'];
     }
 }
 
