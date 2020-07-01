674ab02e4f9d4ed9b68ad6494de953245495eb5b
Dick Brouwers <dick@insided.nl>
2020-07-01 21:18:56 +0200

    refactored controller test

--- test/Controller/CalculateDistanceActionTest.php
+++ test/Controller/CalculateDistanceActionTest.php
@@ -13,21 +13,27 @@ final class CalculateDistanceActionTest extends TestCase
 {
     public function testItCanCalculateTheSumOfTwoDistances()
     {
-        $action = new CalculateDistanceAction(
-            new DistanceCalculator()
-        );
-        $command = new CalculateDistance(
+        $action = new CalculateDistanceAction(new DistanceCalculator());
+
+        /** @var Message $response */
+        $response = $action($this->getCommand());
+
+        self::assertInstanceOf(Message::class, $response);
+        self::assertEquals(['application/json'], $response->getHeader('Content-Type'));
+        self::assertEquals((string) $response->getBody(), $this->expectedContent());
+    }
+
+    private function getCommand(): CalculateDistance
+    {
+        return new CalculateDistance(
             'meter',
             ['measure' => 'meter', 'distance' => 1.0],
             ['measure' => 'meter', 'distance' => 1.0]
         );
+    }
 
-        /** @var Message $response */
-        $response = $action->__invoke($command);
-        $expectedContent = json_encode(['measure' => 'meter', 'distance' => 2.0]);
-
-        self::assertInstanceOf(Message::class, $response);
-        self::assertEquals(['application/json'], $response->getHeader('Content-Type'));
-        self::assertEquals((string) $response->getBody(), $expectedContent);
+    private function expectedContent(): string
+    {
+        return json_encode(['measure' => 'meter', 'distance' => 2.0]);
     }
 }
---
+++ test/E2EDistanceCalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Assignment;
+
+use PHPUnit\Framework\TestCase;
+
+final class E2EDistanceCalculatorTest extends TestCase
+{
+
+}
