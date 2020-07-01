f2d70cc9b20b0cdf852c55ab78f3cdf2436a9c87
Dick Brouwers <dick@insided.nl>
2020-07-01 19:05:08 +0200

    green

--- src/DistanceCalculator.php
+++ src/DistanceCalculator.php
@@ -8,8 +8,13 @@ final class DistanceCalculator
 {
     public function calculate(array $distance1, array $distance2, string $measure): float
     {
+        if ($distance1['measure'] === 'yard' && $distance2['measure'] == 'meter' && $measure === "meter") {
+            $distance1['measure'] = 'meter';
+            $distance1['distance'] *= 0.9144;
+        }
+
         $sum = $distance1['distance'] + $distance2['distance'];
-        if ($measure === 'yard') {
+        if ($distance1['measure'] === 'meter' && $distance2['measure'] == 'meter' && $measure === 'yard') {
             $sum *= 1.093613;
         }
 
