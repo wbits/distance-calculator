d8f727f0f4378eb1ade885a64bacb463401abeeb
Dick Brouwers <dick@insided.nl>
2020-07-02 13:26:41 +0200

    refactored e2e test allowing more scenarios to run through dataprovider

--- test/E2EDistanceCalculatorTest.php
+++ test/E2EDistanceCalculatorTest.php
@@ -9,18 +9,35 @@ use PHPUnit\Framework\TestCase;
 
 final class E2EDistanceCalculatorTest extends TestCase
 {
-    public function testItCanMakeARequestToCalculateTheSumOfDistances()
+    /**
+     * @dataProvider provider()
+     */
+    public function testItCanMakeARequestToCalculateTheSumOfDistances(
+        array $d1,
+        array $d2,
+        string $expectedContent,
+        int $expectedStatusCode
+    )
     {
         $httpClient = new Client();
         $uri = 'http://localhost:2323/calculate/meter';
-        $d1 = ['measure'  => 'meter', 'distance' => 1.0];
-        $d2 = ['measure'  => 'meter', 'distance' => 2.0];
-        $expected = ['measure'  => 'meter', 'distance' => 3.0];
         $jsonPayload = ['distances' => [$d1, $d2]];
 
         $response = $httpClient->request('POST', $uri, ['json' => $jsonPayload]);
 
-        self::assertEquals(200, $response->getStatusCode());
-        self::assertEquals(json_encode($expected), $response->getBody()->getContents());
+        self::assertEquals($expectedStatusCode, $response->getStatusCode());
+        self::assertEquals($expectedContent, $response->getBody()->getContents());
+    }
+
+    public function provider(): array
+    {
+        return [
+            [
+                'd1' => ['measure'  => 'meter', 'distance' => 1.0],
+                'd2' => ['measure'  => 'meter', 'distance' => 2.0],
+                'expectedContent' => json_encode(['measure'  => 'meter', 'distance' => 3.0]),
+                'expectedStatusCode' => 200,
+            ]
+        ];
     }
 }
