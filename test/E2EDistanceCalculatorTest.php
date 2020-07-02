5fd3d5269795bb6518a6441796ff3743224adb1f
Dick Brouwers <dick@insided.nl>
2020-07-02 11:42:41 +0200

    red, it can make an http request to calcaluate the sum of distances

--- test/E2EDistanceCalculatorTest.php
+++ test/E2EDistanceCalculatorTest.php
@@ -1,12 +1,26 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Assignment;
 
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 final class E2EDistanceCalculatorTest extends TestCase
 {
+    public function testItCanMakeARequestToCalculateTheSumOfDistances()
+    {
+        $httpClient = new Client();
+        $uri = 'http://localhost:2323/calculate/meters';
+        $d1 = ['measure'  => 'meter', 'distance' => 1.0];
+        $d2 = ['measure'  => 'meter', 'distance' => 2.0];
+        $expected = ['measure'  => 'meter', 'distance' => 3.0];
+        $jsonPayload = ['distances' => [$d1, $d2,]];
 
+        $response = $httpClient->request('POST', $uri, ['json' => $jsonPayload]);
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals(json_encode($expected), $response->getBody()->getContents());
+    }
 }
--- web/index.php
+++ web/index.php
@@ -1,1 +1,3 @@
 <?php
+
+echo "I am a distance calculator";
